782173a4a3c81f0a78a0ef817cbd1ff82322c253
Kaan Taze <bkaantaze@gmail.com>
2021-03-20 17:51:49 +0300

    basic project structure with Catch2 testing

---
+++ C++/build/src/CMakeFiles/intro2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/intro2.dir/main.cpp.o"
+  "intro2"
+  "intro2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/intro2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
