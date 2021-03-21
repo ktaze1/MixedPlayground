bde706ed82495e3641e830bd1da3f175b33550e1
Kaan Taze <bkaantaze@gmail.com>
2021-03-21 09:24:17 +0300

    Finishing bit manipulation

--- C++/src/4-BitManipulation.cpp
+++ C++/src/4-BitManipulation.cpp
@@ -1,6 +1,8 @@
 // Remove rightmost bit
-#include <type_traits>
 #include <bitset>
+#include <type_traits>
+#include <iostream>
+
 template <typename T> T rightmostSetBitRemoved(T n) {
   static_assert(std::is_integral<T>::value && !std::is_signed<T>::value,
                 "Type should be unsigned!");
@@ -10,10 +12,19 @@ template <typename T> T rightmostSetBitRemoved(T n) {
 }
 
 // set all bits
-std::bitset<10> x; 
-x.set(); // set all to 1
-x.flip(); //flips all bits
-x.flip(2); //flips the 3rd rmb
+void setBits() {
+  std::bitset<10> x;
+  x.set();   // set all to 1
+  x.set(2, true); // set nth to x
+  x.flip();  // flips all bits
+  x.flip(2); // flips the 3rd rmb
+}
 
+// small to capital letters with bit manipulation
 
-// to be continued
+const void setCharToCapital(){
+  char op1 = 'a'; //        01 1 00001
+  int mask = 0xDF; //mask = 11 0 11111
+
+  std::cout << (op1 & mask); // A = 01000001
+}
