794b0f33a89714b18d53a1093dd6db555f314b6e
Kaan Taze <bkaantaze@gmail.com>
2021-03-22 22:28:52 +0300

    finishing pointers

--- C++/src/14-Pointers.cpp
+++ C++/src/14-Pointers.cpp
@@ -1,4 +1,7 @@
+#include <cstddef>
 #include <iostream>
+#include <memory>
+#include <vector>
 
 void pointerOps() {
   int var = 20;
@@ -59,9 +62,9 @@ public:
 
 int Class4::i = 1;
 
-int j = 2; //another global variable
+int j = 2; // another global variable
 
-int main(){
+int main1() {
   Fn *fn;
 
   fn = &MyFn; // point to one function
@@ -95,7 +98,6 @@ int main(){
   p_C_i = &Class3::x; // point to x within any Class
   int i = c3.*p_C_i;  // use p_c_i to fetch x from c3's instance
 
-
   //-------------
 
   int k = 3; // local variable
@@ -108,8 +110,74 @@ int main(){
   ptr = &j; // point to j
   *ptr = 5; // modify j
 
-  ptr = &Class4::i; //point to Class4::i
-  *ptr = 4; //modify it.
+  ptr = &Class4::i; // point to Class4::i
+  *ptr = 4;         // modify it.
 }
 
-// To Be Continued...
+// unique pointers
+
+// dynamic obj is owned by only one instance at any time
+
+void unqPtr() {
+  std::unique_ptr<int> ptr = std::make_unique<int>(20);
+  // create dynamic int with value of 20 owned by unique ptr
+
+  std::unique_ptr<int[]> ptrArray = std::make_unique<int[]>(15);
+
+  // use std::move( newSmartPointer ) to transfer ownership
+
+  std::unique_ptr<int> newOwner = std::move(ptr);
+  // ptr becomes nullptr
+
+} // out of scope, unique ptr calls the destructor
+
+// shared pointers
+
+class Foo {};
+
+void sharedPointers() {
+
+  std::shared_ptr<Foo> firstShared = std::make_shared<Foo>();
+  std::shared_ptr<Foo> secondShared(firstShared); // copy constructor
+  secondShared = firstShared;                     // assignment
+
+  std::shared_ptr<int[]> oldSh(
+      new int[10],
+      std::default_delete<int[]>()); // before c++17 you gotta specify deleter
+
+  std::shared_ptr<int[]> sh(new int[10]); // this is c++17
+  sh[0] = 42;
+
+  // shared pointers can point to sub-obj
+  struct Foo2 {
+    int x;
+  };
+  std::shared_ptr<Foo2> p1 = std::make_shared<Foo2>();
+  std::shared_ptr<int> p2(p1, &p1->x);
+
+  // if p1 goes out of scope underlying Foo2 obj will still be alive
+}
+
+// sharing with temp ownership:
+
+// weak_ptr: can point to obj owned by instances of shared_ptr while only
+// becoming temp owners, this means weak pointers do not alter the obj's
+// reference count and therefore do not prevent an obj's deletion 
+
+struct TreeNode{
+  std::weak_ptr<TreeNode> parent;
+  std::vector< std::shared_ptr<TreeNode> > children;
+};
+
+int main(){
+
+ std::shared_ptr<TreeNode> root(new TreeNode); // create as root
+
+ for(size_t i = 0; i < 100; ++i) { // give parent 100 child nodes
+   std::shared_ptr<TreeNode> child(new TreeNode);
+   root->children.push_back(child);
+   child->parent = root;
+ }
+
+ root.reset(); //all the childs destroyed
+}
