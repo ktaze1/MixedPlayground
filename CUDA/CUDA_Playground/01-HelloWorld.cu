d2c4106fc8bd44022fd75317a10270d3fca63a5c
Kaan Taze <bkaantaze@gmail.com>
2021-03-27 23:31:37 +0300

    uncommenting rest of the files

--- CUDA/CUDA_Playground/01-HelloWorld.cu
+++ CUDA/CUDA_Playground/01-HelloWorld.cu
@@ -1,17 +1,17 @@
-﻿//#include <stdio.h>
-//#include <stdlib.h>
-//#include <device_launch_parameters.h>
-//#include <cuda_runtime.h>
-//
-//__global__ void print_from_gpu(void) {
-//    printf("Hello World! from thread [%d, %d] \
-//        From device \n",
-//        threadIdx.x, blockIdx.x);
-//}
-//
-//int main(void) {
-//    printf("Hello World from host!\n");
-//    print_from_gpu<<<1,2>>> ();
-//    cudaDeviceSynchronize();
-//    return 0;
-//}
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <device_launch_parameters.h>
+#include <cuda_runtime.h>
+
+__global__ void print_from_gpu(void) {
+    printf("Hello World! from thread [%d, %d] \
+        From device \n",
+        threadIdx.x, blockIdx.x);
+}
+
+int main(void) {
+    printf("Hello World from host!\n");
+    print_from_gpu<<<1,2>>> ();
+    cudaDeviceSynchronize();
+    return 0;
+}
